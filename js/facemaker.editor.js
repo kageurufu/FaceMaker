683814cefb99053000f8b8796d075dbbb6b5ea15
Frank Tackitt <franklyn@tackitt.net>
2014-10-09 21:31:45 -0700

    Updates to the editor. Got shapes mostly working
    Got colors working, thanks to the help of @martypdx
    
    Theres still a bunch of debugging being dumped out
    
    As far as my tests showed, the downloaded faces are still
    100% compatible with Facer (only tested on a Moto 360).
    
    Still no support for square faces, but thats mostly just redefining the
    mask I think. Might be some changes to X/Y/width/height due to different
    resolutions. I need to download some Square faces from facerepo and
    check

--- js/facemaker.editor.js
+++ js/facemaker.editor.js
@@ -1,4 +1,37 @@
 (function (FM) {
+  
+  function pad(str, len) {
+    while (str.length < length) str = '0' + str;
+    return str;
+  };
+
+  FM.prototype.hex_to_argb = function(hex) {
+    if(hex === undefined) { return 0; };
+
+    hex = hex.replace("#", "");
+    if(hex.length == 6) {
+      hex = "FF" + hex;
+    };
+
+    var a = "0x" + hex.substring(0, 2),
+        r = "0x" + hex.substring(2, 4),
+        g = "0x" + hex.substring(4, 6),
+        b = "0x" + hex.substring(6, 8);
+
+    return (parseInt(a) << 24) | (parseInt(r) << 16) | (parseInt(g) << 8) | parseInt(b);
+  };
+
+  FM.prototype.argb_to_hex = function(argb) {
+    var a = argb >> 24 & 0xFF,
+        r = argb >> 16 & 0xFF,
+        g = argb >> 8  & 0xFF,
+        b = argb       & 0xFF;
+
+    return "#" + 
+            pad(r.toString(16), 2) +
+            pad(g.toString(16), 2) +
+            pad(b.toString(16), 2);
+  };
 
   FM.prototype.init_editor = function() {
     var fm = this;
@@ -12,6 +45,23 @@
 
       fm.editor = {};
       
+      Ractive.components.color = Ractive.extend({
+        template: "<input type='color' value='{{hex}}'>",
+        computed: {
+          hex: {
+            get: function() {
+              var hex = fm.argb_to_hex(this.get('argb'));
+              console.log(hex);
+              return hex;
+            },
+            set: function(hex) {
+              console.log(hex);
+              this.set('argb', fm.hex_to_argb(hex));
+            }
+          }
+        }
+      });
+
       fm.editor.ractive = new Ractive({
         el: fm.options.editor,
         template: '#editor-template',
@@ -25,19 +75,12 @@
         this.set(k, parseInt(n));
       }
 
-      function intColorObserv(n, o, k) {
-        this.set(k, n);
-      }
-
       r.observe(
-        'face.watchface.*.alignment face.watchface.*.transform', 
+        'face.watchface.*.alignment face.watchface.*.transform face.watchface.*.shape_type ' +
+        'face.watchface.*.shape_opt', 
         intObserv.bind(r)
       );
 
-      r.observe(
-        'face.watchface.*.color face.watchface.*.bgcolor face.watchface.*.low_power_color', 
-        intColorObserv.bind(r)
-      );
     }
     return true;
   };
